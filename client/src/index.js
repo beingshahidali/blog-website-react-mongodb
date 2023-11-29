8d01e1148ab33f470b690d835c6013b6c8c2d81d
Shahid Ali <beingshahidali@gmail.com>
2023-11-29 23:35:36 +0530

    .

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import { ContextProvider } from "./context/Context";
 
-ReactDOM.render(
+const root = document.getElementById("root");
+const appRoot = createRoot(root);
+
+appRoot.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <ContextProvider>
+      <App />
+    </ContextProvider>
+  </React.StrictMode>
 );
